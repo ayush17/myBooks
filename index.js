d2ae23a593d30022a0daf09cd01749d74d824310
Ayush Tiwari <ayushtiwari@Ayushs-MacBook-Pro.local>
2023-03-25 00:47:04 +0530

    add cors redirection

--- index.js
+++ index.js
@@ -16,6 +16,14 @@ mongoose
 
 //Middlewares
 app.use(express.json());
+const cors = require("cors");
+const corsOptions = {
+  origin: "*",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions)); // Use this after the variable declaration
+
 // Routes Middleware
 app.use("/api/user", authRoute);
 app.use("/api/posts", postRoute);
